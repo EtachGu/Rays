0631d2d5531aa39074dd51a1031e5de3c2e258dd
EvanGu <detached_gu@163.com>
2018-09-09 18:54:20 +0800

    修改配置服务器设置

--- rays-config/src/main/java/com/evangu/config/ConfigApplication.java
+++ rays-config/src/main/java/com/evangu/config/ConfigApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Rays-Config服务
+ * @author Gu danpeng
+ * @version 1.0.0 2018-9-9
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigApplication {
