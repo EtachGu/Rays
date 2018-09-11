276dbcae78a09e857690fbee8059bd236e14b992
EvanGu <detached_gu@163.com>
2018-09-11 23:57:28 +0800

    update

---
+++ rays-auth/src/main/java/com/evangu/raysauth/RaysAuthApplication.java
@@ -0,0 +1,12 @@
+package com.evangu.raysauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaysAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaysAuthApplication.class, args);
+	}
+}
