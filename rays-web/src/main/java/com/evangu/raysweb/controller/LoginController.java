ea8998f91858f60752d8a21e6c1409229881e6b5
EvanGu <detached_gu@163.com>
2018-09-19 21:22:26 +0800

    add Thymeleaf Template

--- rays-web/src/main/java/com/evangu/raysweb/controller/LoginController.java
+++ rays-web/src/main/java/com/evangu/raysweb/controller/LoginController.java
@@ -1,6 +1,9 @@
 package com.evangu.raysweb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,8 +15,16 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class LoginController {
 
+    @Autowired
+    OAuth2RestTemplate restTemplate;
+
     @RequestMapping("/")
     public ModelAndView index() {
         return new ModelAndView("login");
+    }
+
+    @RequestMapping("/login")
+    public void login(@RequestHeader Object  requestHeader) {
+        restTemplate.getAccessToken();
     }
 }
