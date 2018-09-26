e1ae8a466ec828b3062c4207a4ffdf5148c0a4f5
EvanGu <detached_gu@163.com>
2018-09-26 20:29:25 +0800

    change entity folder

--- rays-auth/src/main/java/com/evangu/raysauth/entity/UserInfo.java
+++ rays-auth/src/main/java/com/evangu/raysauth/entity/UserInfo.java
@@ -1,4 +1,4 @@
-package com.evangu.raysauth.domain;
+package com.evangu.raysauth.entity;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
