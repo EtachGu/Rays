ad6c472973e432dec34f80144ee868858fc45f4d
EvanGu <detached_gu@163.com>
2018-09-30 23:04:55 +0800

    update

---
+++ rays-web/src/main/java/com/evangu/raysweb/config/RaysUsernamePasswordFilter.java
@@ -0,0 +1,12 @@
+package com.evangu.raysweb.config;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * @author: Gu danpeng
+ * @data: 2018-9-30
+ * @version：1.0
+ */
+public class RaysUsernamePasswordFilter extends UsernamePasswordAuthenticationFilter {
+
+}
