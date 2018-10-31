7932203fa6b7e35bc64d00c5690a1325440d97ac
EvanGu <detached_gu@163.com>
2018-10-31 23:43:34 +0800

    add authorize

--- rays-resource/src/main/java/com/evangu/raysresource/service/OrderService.java
+++ rays-resource/src/main/java/com/evangu/raysresource/service/OrderService.java
@@ -1,10 +1,16 @@
 package com.evangu.raysresource.service;
 
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
+
 /**
  * @author: Gu danpeng
  * @data: 2018-10-4
  * @version：1.0
  */
 public interface OrderService {
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @PostFilter("hasPermission(filterObject, 'read') or hasPermission(filterObject, admin)")
     public String getOrder(String id);
 }
