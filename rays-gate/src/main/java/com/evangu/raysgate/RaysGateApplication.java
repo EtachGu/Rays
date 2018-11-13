631faaa197c4d2741470dc54c52ea75189885a61
EvanGu <detached_gu@163.com>
2018-11-13 20:22:36 +0800

    add Rays-Gate

---
+++ rays-gate/src/main/java/com/evangu/raysgate/RaysGateApplication.java
@@ -0,0 +1,12 @@
+package com.evangu.raysgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaysGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaysGateApplication.class, args);
+	}
+}
