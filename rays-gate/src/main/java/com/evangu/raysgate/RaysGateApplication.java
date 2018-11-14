9ab91d855c316b890cdb3c50635209e4208e3543
EvanGu <detached_gu@163.com>
2018-11-14 20:53:26 +0800

    add Zuul

--- rays-gate/src/main/java/com/evangu/raysgate/RaysGateApplication.java
+++ rays-gate/src/main/java/com/evangu/raysgate/RaysGateApplication.java
@@ -2,8 +2,10 @@ package com.evangu.raysgate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class RaysGateApplication {
 
 	public static void main(String[] args) {
