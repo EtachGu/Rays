f748d3d20c91cb890c99af48ecc1ff229809fcd1
EvanGu <detached_gu@163.com>
2018-11-16 23:00:32 +0800

    update

--- rays-gate/src/main/java/com/evangu/raysgate/RaysGateApplication.java
+++ rays-gate/src/main/java/com/evangu/raysgate/RaysGateApplication.java
@@ -2,12 +2,14 @@ package com.evangu.raysgate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableZuulProxy
+@EnableEurekaClient
 public class RaysGateApplication {
 
 	public static void main(String[] args) {
