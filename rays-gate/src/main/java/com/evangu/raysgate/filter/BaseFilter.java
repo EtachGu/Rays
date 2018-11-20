9a524e400a901736ff172d6def5226c354139b2a
EvanGu <detached_gu@163.com>
2018-11-20 23:37:48 +0800

    add Filter

---
+++ rays-gate/src/main/java/com/evangu/raysgate/filter/BaseFilter.java
@@ -0,0 +1,41 @@
+package com.evangu.raysgate.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author: Gu danpeng
+ * @data: 2018-11-20
+ * @version：1.0
+ */
+@Component
+public class BaseFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        final String requestUri = request.getRequestURI();
+        final String method = request.getMethod();
+        System.out.println("====test Zuul Filter" + requestUri);
+        return null;
+    }
+
+}
