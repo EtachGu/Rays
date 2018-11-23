b4418770c9f695eec823efdc7766ea16702d8aef
EvanGu <detached_gu@163.com>
2018-11-23 23:49:21 +0800

    add Redis

--- rays-gate/src/main/java/com/evangu/raysgate/filter/BaseFilter.java
+++ rays-gate/src/main/java/com/evangu/raysgate/filter/BaseFilter.java
@@ -1,7 +1,9 @@
 package com.evangu.raysgate.filter;
 
+import com.evangu.raysgate.jedis.RedisService;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +15,10 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Component
 public class BaseFilter extends ZuulFilter {
+
+    @Autowired
+    private RedisService redisService;
+
     @Override
     public String filterType() {
         return "pre";
@@ -35,6 +41,10 @@ public class BaseFilter extends ZuulFilter {
         final String requestUri = request.getRequestURI();
         final String method = request.getMethod();
         System.out.println("====test Zuul Filter" + requestUri);
+        final String jwt = redisService.get(request.getParameter("access_token"));
+
+        System.out.println("\t JWT:" + jwt);
+        ctx.addZuulRequestHeader("token",jwt);
         return null;
     }
 
